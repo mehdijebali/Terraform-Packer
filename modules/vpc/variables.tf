033ea415415fea71a00b963dd7877fa8dba29f11
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-12-23 10:19:59 +0100

    adjust vpc module variables

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,11 +1,3 @@
 #Define Variable for Custom Module VPC
-
-variable "AWS_REGION" {
-    type    = string
-    default = "us-east-1"
-}
-
-variable "ENVIRONMENT" {
-    type    = string
-    default = ""
-}
+variable "AWS_REGION" {}
+variable "ENVIRONMENT" {}
