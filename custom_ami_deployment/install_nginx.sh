2898a74abe6cd552eaab9824bb8290f400cd4740
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2023-01-04 12:52:32 +0100

    Set up nginx installation

--- custom_ami_deployment/install_nginx.sh
+++ custom_ami_deployment/install_nginx.sh
@@ -1,3 +1,17 @@
 #!/bin/bash
-apt-get update
-apt-get install -y nginx docker.io
+
+sudo yum update -y
+
+# Install nginx
+sudo amazon-linux-extras enable nginx1
+sudo yum clean metadata
+sudo yum -y install nginx
+
+# Install docker
+sudo yum -y install docker
+
+#Start Nginx & docker 
+sudo systemctl enable docker.service
+sudo systemctl enable nginx
+sudo systemctl start docker.service
+sudo systemctl start nginx
